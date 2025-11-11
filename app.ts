f9624084c2d4e30dea1a423c1462664d79166a0d
egwaj <egwajohnson.a@gmail.com>
2025-11-11 20:47:14 +0100

    Describe what you changed

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import { PORT } from './config/system.variance';
+import router from "./route/app.route";
+
+const app = express();
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
