583ff011f96efb8d6c47f7815c056b634adaefd2
egwaj <egwajohnson.a@gmail.com>
2025-11-13 18:20:41 +0100

    feat: model

--- src/models/otp.model.ts
+++ src/models/otp.model.ts
@@ -1,6 +1,5 @@
 import mongoose, { Schema, Types } from "mongoose";
 
-<<<<<<< HEAD
 const otpSchema: Schema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User" },
@@ -9,15 +8,6 @@ const otpSchema: Schema = new Schema(
     createdAt: { type: Date, default: Date.now, expires: "1hr" }, // OTP expires after 1 hour
     updatedAt: { type: Date, default: Date.now },
     //   expiresAt: { type: Date, default: Date.now },
-=======
-const otpSchema = new Schema(
-  {
-    userId: { type: Types.ObjectId, require: true, ref: "User" },
-    email: { type: String, require: true },
-    otp: { type: String, require: true },
-    createdAt: { type: Date, default: Date.now, expires: "1hr" },
-    updatedAt: { type: Date, default: Date.now },
->>>>>>> 7dd47fd7a790e6a1224c66c3067cc8e72f8e117d
   },
   { timestamps: true }
 );
